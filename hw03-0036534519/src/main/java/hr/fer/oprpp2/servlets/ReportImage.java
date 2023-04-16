e53e39a2c0ec4300b4de5b769fdff2e77cf62939
MarkoMiljkovic45 <miljkovicmarko45@gmail.com>
2023-04-16 21:29:42 +0200

    Task 4

---
+++ hw03-0036534519/src/main/java/hr/fer/oprpp2/servlets/ReportImage.java
@@ -0,0 +1,49 @@
+package hr.fer.oprpp2.servlets;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@WebServlet(name="reportImage", urlPatterns={"/reportImage"})
+public class ReportImage extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("image/png");
+
+        OutputStream outputStream = resp.getOutputStream();
+
+        JFreeChart chart = getChart();
+        int width = 500;
+        int height = 350;
+        ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+    }
+
+    private static JFreeChart getChart() {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("Linux", 23.3);
+        dataset.setValue("Mac", 32.4);
+        dataset.setValue("Windows", 44.2);
+
+        boolean legend = true;
+        boolean tooltips = false;
+        boolean urls = false;
+
+        JFreeChart chart = ChartFactory.createPieChart("OS usage", dataset, legend, tooltips, urls);
+
+        chart.setBorderPaint(Color.GREEN);
+        chart.setBorderStroke(new BasicStroke(5.0f));
+        chart.setBorderVisible(true);
+
+        return chart;
+    }
+}
---
+++ hw03-0036534519/src/main/webapp/report.jsp
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>OS Usage</title>
+  </head>
+  <body>
+    <h1>OS usage</h1>
+
+    <p>
+      Here are the results of OS usage in survey that we completed.
+    </p>
+
+    <img src="${pageContext.request.contextPath}/reportImage" alt="OS-usage"/>
+  </body>
+</html>
